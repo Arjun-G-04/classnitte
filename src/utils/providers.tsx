4fc805028ce759908b3d259e0f6b945bfdff937a
Arjun <34741958+Arjun-G-04@users.noreply.github.com>
2024-06-15 11:01:12 +0530

    fix: prod fix

--- src/utils/providers.tsx
+++ src/utils/providers.tsx
@@ -11,7 +11,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					url: "http://localhost:3000/api/trpc",
+					url: `${process.env.NODE_ENV === "production" ? "https://classnitte.vercel.app" : "http://localhost:3000"}/api/trpc`,
 				}),
 			],
 		}),
