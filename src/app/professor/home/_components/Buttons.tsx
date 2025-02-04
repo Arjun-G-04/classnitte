e59c0a62699370ccf178abf6bc5583be3717eb47
Aritra Dey <adey01027@gmail.com>
2025-02-04 18:10:45 +0530

    refactor: use async function for signOut in CustomButton

--- src/app/professor/home/_components/Buttons.tsx
+++ src/app/professor/home/_components/Buttons.tsx
@@ -8,18 +8,19 @@ import toast from "react-hot-toast";
 export default function Buttons() {
 	const router = useRouter();
 
+	const handleLogout = async () => {
+		const loadingToast = toast.loading("Logging out...");
+		try {
+			await signOut();
+			toast.success("Logged out successfully", { id: loadingToast });
+		} catch (error) {
+			toast.error("Error logging out", { id: loadingToast });
+		}
+	};
+
 	return (
 		<div className="flex flex-row gap-5">
-			<CustomButton
-				type="sync"
-				text="Logout"
-				syncFn={() => {
-					const loadingToast = toast.loading("Logging out...");
-					signOut().then(() => {
-						toast.success("Logged out successfully", { id: loadingToast });
-					});
-				}}
-			/>
+			<CustomButton type="sync" text="Logout" syncFn={handleLogout} />
 			<CustomButton
 				type="sync"
 				text="Create Class"
